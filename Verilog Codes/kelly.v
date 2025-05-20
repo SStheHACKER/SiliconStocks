43a992bb525aa677c91e41eebd80b9b74f49369a
Naveen120606 <iitianaveen@gmail.com>
2025-05-20 17:46:39 +0530

    Update kelly.v

--- Verilog Codes/kelly.v
+++ Verilog Codes/kelly.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 17.04.2025 13:16:01
+// Create Date: 19.04.2025 23:58:04
 // Design Name: 
 // Module Name: kelly
 // Project Name: 
@@ -24,17 +24,17 @@ module kelly(
     input clk,
     input rst,
     input enable,
-    input [15:0] data_in, 
+    input [7:0] data_in, 
     input RSI_buy, EMA_buy, Momentum_buy,
     input RSI_sell, EMA_sell, Momentum_sell,
     output reg [9:0] bought,
-    output reg [15:0] profit,
+    output reg [13:0] profit,
     output [1:0] stock_id
 );
 
-    assign stock_id = data_in[15:14];
-    wire [13:0] price_raw = data_in[13:0];
-    wire [15:0] stock_price = {2'b00, price_raw}; 
+    wire stock_id = data_in[7:6];
+    wire [5:0] price_raw = data_in[5:0];
+    wire [7:0] stock_price = {2'b00, price_raw}; 
 
     reg [9:0] total_stocks = 100;
     reg [15:0] money = 25000;
