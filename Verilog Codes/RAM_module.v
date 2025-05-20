c67a33a9e4bf435fca4aed49dfe5b2d7748ff786
Naveen120606 <iitianaveen@gmail.com>
2025-05-20 17:45:16 +0530

    Update RAM_module.v

--- Verilog Codes/RAM_module.v
+++ Verilog Codes/RAM_module.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 16.04.2025 23:54:22
+// Create Date: 12.04.2025 16:20:55
 // Design Name: 
 // Module Name: RAM_module
 // Project Name: 
@@ -21,28 +21,32 @@
 
 
 module RAM_module 
-#(parameter msg_width = 16,
-parameter mem_height = 32,
+#(parameter msg_width = 8,
+parameter mem_height = 8,
 parameter addr = 5
 )
 (input clk,
 input rst,
 input we,
+//input enable,
 input [addr-1:0] w_addr,
 input [msg_width-1:0] data_in,
 output reg [msg_width-1:0] data_out
     );
     
+//    reg[addr-1:0] w_addr;
     reg[addr-1:0] r_addr;
     reg [msg_width-1:0] mem [0:mem_height-1]; 
     always @(negedge clk) begin
         if(rst) begin
-            data_out<=16'b0;
+            data_out<=8'b0;
             r_addr<=5'b0;
+//            w_addr<=5'b0;
         end
         else begin
             if(we) begin
                 mem[w_addr] <= data_in;
+//                w_addr <= w_addr + 1'b1;
             end 
             else begin
                 data_out <= mem[r_addr];
